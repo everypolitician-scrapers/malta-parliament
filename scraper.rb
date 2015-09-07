48bb0a56a60f2bf8d8894652c8f20ef9dd1a74c3
Tony Bowden <tony@tmtm.com>
2015-09-07 16:53:20 +0100

    Tidy faction information

--- scraper.rb
+++ scraper.rb
@@ -32,13 +32,14 @@ def scrape_person(url)
   data = { 
     id: url.to_s.split('/').last,
     name: box.css('h1').text.split(' - ').first.sub('Hon. ','').sub(' MP','').tidy,
-    faction: box.xpath('.//p[strong[contains(.,"Parliamentary Group:")]]//img/@title').text,
+    faction: box.xpath('.//strong[contains(.,"Parliamentary Group")]/..//img/@title').text,
     email: box.css('a[href*="mailto:"]/@href').text.split('mailto:').drop(1).first,
     image: box.css('img/@src').first.text,
     term: 12,
     source: url.to_s,
   }
   data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
+  data[:faction] = 'Partit Nazzjonlista' if data[:faction] == 'PN'
   ScraperWiki.save_sqlite([:id, :term], data)
 end
 
