f9c80eefffe67a62914142e5cbb4dbbdbc765a4a
Oliver Denman <oliver@therewillbeanimals.com>
2017-01-13 15:55:20 +0000

    Capture start date

--- scraper.rb
+++ scraper.rb
@@ -47,6 +47,15 @@ class MemberPage < Scraped::HTML
     url.to_s
   end
 
+  field :start_date do
+    date_from(
+      recent_electoral_history
+                  .scan(/(?:Elected on|Oath of Allegiance|casual elections on)[^:]*\:\s*(\d{1,2}\.\d{1,2}\.\d{2,4})/)
+                  .flatten
+                  .last
+    )
+  end
+
   field :end_date do
     date_from(
       recent_electoral_history
