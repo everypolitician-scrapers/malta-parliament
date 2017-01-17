6fe0ae6d60a7e51a9a2995a986be7c88563f1015
Oliver Denman <oliver@therewillbeanimals.com>
2017-01-17 15:51:13 +0000

    Test that member has the correct start date

---
+++ tests/electoral_data_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require_relative './test_helper'
+require_relative '../lib/member_page.rb'
+
+describe MemberPage do
+  around { |test| VCR.use_cassette('MemberPage', &test) }
+
+  subject do
+    url = 'http://www.parlament.mt/vella-george'
+    MemberPage.new(response: Scraped::Request.new(url: url).response)
+  end
+
+  describe 'electoral data' do
+    it 'should have the correct start date' do
+      subject.start_date.must_equal '2013-03-11'
+    end
+  end
+end
