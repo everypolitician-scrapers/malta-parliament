5a1f3c5ecc926840cd7d36f746a625b624e6bb2c
Oliver Denman <oliver@therewillbeanimals.com>
2017-01-20 19:15:43 +0000

    Create ElectoralHistory fragment class

---
+++ lib/electoral_history.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+require 'scraped'
+
+class ElectoralHistory < Scraped::HTML
+  def to_h
+    data = noko.text
+               .split(Regexp.union(term_headings))
+               .map(&:tidy).reject(&:empty?)
+               .map { |str| str.delete(':') }
+               .map { |str| str.split(/(\d\d?\.\d\d?\.\d\d(?:\d\d)?)/).map(&:tidy) }
+               .map { |arr| Hash[*arr] }
+
+    term_headings.map { |str| str.delete(':') }
+                 .map(&:tidy)
+                 .zip(data).to_h
+  end
+
+  private
+
+  def term_headings
+    noko.first
+        .text
+        .scan(/([A-Z][a-z]+(?:st|nd|rd|th)\sParliament\s?:?)/)
+        .flatten
+        .map(&:tidy)
+  end
+end
