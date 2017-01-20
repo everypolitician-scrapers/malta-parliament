b418e445b9edc997854a48569228de3c56d823ca
Oliver Denman <oliver@therewillbeanimals.com>
2017-01-20 18:38:03 +0000

    Add ExMemberPage
    Subclasses MemberPage. Returns populate :electoral_history.

---
+++ lib/ex_member_page.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require 'scraped'
+require_relative 'electoral_history'
+
+class ExMemberPage < MemberPage
+  field :electoral_history do
+    (fragment noko.xpath('//td[contains(.,"Electoral History")]/following-sibling::td') => ElectoralHistory).to_h
+  end
+end
