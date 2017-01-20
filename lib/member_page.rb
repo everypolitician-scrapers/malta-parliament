569c386f4fcd73b2e15677e66554d63e8acec03d
Oliver Denman <oliver@therewillbeanimals.com>
2017-01-20 18:22:40 +0000

    Return empty hash for electoral history

--- lib/member_page.rb
+++ lib/member_page.rb
@@ -31,30 +31,10 @@ class MemberPage < Scraped::HTML
   end
 
   field :electoral_history do
-    data = electoral_history_section.text
-                                    .split(Regexp.union(term_headings))
-                                    .map(&:tidy).reject(&:empty?)
-                                    .map { |str| str.delete(':') }
-                                    .map { |str| str.split(/(\d\d?\.\d\d?\.\d\d(?:\d\d)?)/).map(&:tidy) }
-                                    .map { |arr| Hash[*arr] }
-    term_headings.map { |str| str.delete(':') }
-                 .map(&:tidy)
-                 .zip(data).to_h
+    {}
   end
 
   private
-
-  def electoral_history_section
-    noko.xpath('//td[contains(.,"Electoral History")]/following-sibling::td')
-  end
-
-  def term_headings
-    electoral_history_section.first
-                             .text
-                             .scan(/([A-Z][a-z]+(?:st|nd|rd|th)\sParliament\s?:?)/)
-                             .flatten
-                             .map(&:tidy)
-  end
 
   def box
     noko.css('div.column2')
