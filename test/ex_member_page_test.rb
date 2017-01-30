2df787c5615b1191c34bb0daf29a0395d1ee691a
Oliver Denman <oliver@therewillbeanimals.com>
2017-01-30 15:33:42 +0000

    Use ex-member as ExMemberPage test subject

--- test/ex_member_page_test.rb
+++ test/ex_member_page_test.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 require_relative './test_helper'
 require_relative '../lib/ex_member_page.rb'
+require_relative '../lib/decorators/formatted_dates'
+require_relative '../lib/electoral_history'
 
 describe MemberPage do
-  around { |test| VCR.use_cassette('GeorgeVella', &test) }
+  around { |test| VCR.use_cassette('JoeCassar', &test) }
 
   subject do
-    url = 'http://www.parlament.mt/vella-george'
+    url = 'http://www.parlament.mt/joe-cassar'
     ExMemberPage.new(response: Scraped::Request.new(url: url).response)
   end
 
   it 'should have a name field with the name of the member' do
-    subject.name.must_equal 'George Vella'
+    subject.name.must_equal 'Dr Joe Cassar'
   end
 
   it 'should respond to electoral_history' do
