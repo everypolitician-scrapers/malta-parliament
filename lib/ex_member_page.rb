af9f8fa87b902adb7865352cf1a309cef040a76f
Oliver Denman <oliver@therewillbeanimals.com>
2017-01-30 15:34:45 +0000

    Capture electoral history of ex members

--- lib/ex_member_page.rb
+++ lib/ex_member_page.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 require 'scraped'
 require_relative './member_page'
+require_relative './electoral_history'
 
 class ExMemberPage < MemberPage
+  field :electoral_history do
+    (fragment noko.xpath('//td[contains(.,"Electoral History")]/following-sibling::td') => ElectoralHistory).to_h
+  end
 end
