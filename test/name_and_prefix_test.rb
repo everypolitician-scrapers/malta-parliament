82c7379216843ebc4bf69aea8281cfb3bfb8b688
Oliver Denman <oliver@therewillbeanimals.com>
2017-01-30 15:44:21 +0000

    Test that name does not include prefix

---
+++ test/name_and_prefix_test.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+require_relative './test_helper'
+require_relative '../lib/member_page.rb'
+
+describe MemberPage do
+  around { |test| VCR.use_cassette('AlbertFenech', &test) }
+
+  subject do
+    url = 'http://www.parlament.mt/fenech-albert'
+    MemberPage.new(response: Scraped::Request.new(url: url).response)
+  end
+
+  it 'should have a name field with the name of the member without a prefix' do
+    subject.name.must_equal 'Albert Fenech'
+  end
+end
