6b919400a732dc862f14c628aecb551206778bb5
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-07 14:41:05 +0000

    Test member without honorific prefixes

--- test/name_and_prefix_test.rb
+++ test/name_and_prefix_test.rb
@@ -3,22 +3,41 @@ require_relative './test_helper'
 require_relative '../lib/member_page.rb'
 
 describe MemberPage do
-  around { |test| VCR.use_cassette('AlbertFenech', &test) }
+  describe 'Dr Albert Fenech' do
+    around { |test| VCR.use_cassette('AlbertFenech', &test) }
 
-  subject do
-    url = 'http://www.parlament.mt/fenech-albert'
-    MemberPage.new(response: Scraped::Request.new(url: url).response)
+    subject do
+      url = 'http://www.parlament.mt/fenech-albert'
+      MemberPage.new(response: Scraped::Request.new(url: url).response)
+    end
+
+    it 'should have a name field with the name of the member without a prefix' do
+      subject.name.must_equal 'Albert Fenech'
+    end
+
+    it 'should have an honorific_prefix field' do
+      subject.must_respond_to(:honorific_prefix)
+    end
+
+    it 'should store any titles in the honorific_prefix field' do
+      subject.honorific_prefix.must_equal 'Dr'
+    end
   end
 
-  it 'should have a name field with the name of the member without a prefix' do
-    subject.name.must_equal 'Albert Fenech'
-  end
+  describe 'Ian Borg' do
+    around { |test| VCR.use_cassette('IanBorg', &test) }
 
-  it 'should have an honorific_prefix field' do
-    subject.must_respond_to(:honorific_prefix)
-  end
+    subject do
+      url = 'http://www.parlament.mt/borg-ian'
+      MemberPage.new(response: Scraped::Request.new(url: url).response)
+    end
 
-  it 'should store any titles in the honorific_prefix field' do
-    subject.honorific_prefix.must_equal 'Dr'
+    it 'should leave a non prefixed name unchanged' do
+      subject.name.must_equal 'Ian Borg'
+    end
+
+    it 'should not return a prefix when none is present in the name' do
+      subject.honorific_prefix.must_be_empty
+    end
   end
 end
