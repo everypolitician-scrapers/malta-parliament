34f523f34c566078f012b80dcc6de7bc20185307
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-07 14:06:10 +0000

    Test for electoral history in MemberPage instead of ExMemberPage

--- test/ex_member_page_test.rb
+++ test/ex_member_page_test.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 require_relative './test_helper'
-require_relative '../lib/ex_member_page.rb'
+require_relative '../lib/member_page.rb'
 
 describe MemberPage do
   around { |test| VCR.use_cassette('JoeCassar', &test) }
 
   subject do
     url = 'http://www.parlament.mt/joe-cassar'
-    ExMemberPage.new(response: Scraped::Request.new(url: url).response)
+    MemberPage.new(response: Scraped::Request.new(url: url).response)
   end
 
   it 'should have a name field with the name of the member' do
